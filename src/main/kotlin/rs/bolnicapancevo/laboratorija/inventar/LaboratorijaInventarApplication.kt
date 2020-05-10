8cbfcbe11b3b521d6f76f73f0b798aa4db2c6094
Luka Jovicic <luka.jovicic16@gmail.com>
2020-05-10 02:17:27 +0200

    Initial commit

---
+++ src/main/kotlin/rs/bolnicapancevo/laboratorija/inventar/LaboratorijaInventarApplication.kt
@@ -0,0 +1,11 @@
+package rs.bolnicapancevo.laboratorija.inventar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LaboratorijaInventarApplication
+
+fun main(args: Array<String>) {
+    runApplication<LaboratorijaInventarApplication>(*args)
+}
