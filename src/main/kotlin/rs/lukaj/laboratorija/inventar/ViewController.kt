85be491bb603e24faa0e9866634be25281ea3798
Luka Jovicic <luka.jovicic16@gmail.com>
2020-09-20 16:52:01 +0200

    Add report page

--- src/main/kotlin/rs/lukaj/laboratorija/inventar/ViewController.kt
+++ src/main/kotlin/rs/lukaj/laboratorija/inventar/ViewController.kt
@@ -11,16 +11,20 @@ import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
 import java.time.Month
 import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 @Controller
 class ViewController(@Autowired val service: CrudService) {
     companion object {
         val LOGGER: Logger = LoggerFactory.getLogger(ViewController::class.java)
         val BEGGINING_OF_TIME: LocalDate = LocalDate.of(1970, Month.JANUARY, 1)
+
+        val DASH_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy")!!
+        val SLASH_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy")!!
     }
 
     @GetMapping("/")
@@ -33,10 +37,10 @@ class ViewController(@Autowired val service: CrudService) {
                   @DateTimeFormat(pattern="dd-MM-yyyy") date: LocalDate, model: Model) : String {
         val inventories = service.getAllInventories()
         val data = if(date == BEGGINING_OF_TIME) {
-            model["date"] = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            model["date"] = LocalDate.now().format(SLASH_FORMATTER)
             service.getAllItems()
         } else {
-            model["date"] = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            model["date"] = date.format(SLASH_FORMATTER)
             service.getAllItems(date)
         }
         model["data"] = data
@@ -80,8 +84,8 @@ class ViewController(@Autowired val service: CrudService) {
                         @RequestParam(required = false, defaultValue = "") inventory: String,
                          model: Model) : String {
         val data = service.getAllChangesSince(date, until, inventory)
-        val dateStr = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        val untilStr = until.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+        val dateStr = date.format(DASH_FORMATTER)
+        val untilStr = until.format(DASH_FORMATTER)
         model["data"] = data
         model["aggregated"] = false
         model["priceAggregate"] = false
@@ -129,9 +133,37 @@ class ViewController(@Autowired val service: CrudService) {
         return "changes"
     }
 
+    @GetMapping("/report")
+    fun report(@RequestParam(required = false, defaultValue = "01-01-1970")
+               @DateTimeFormat(pattern="dd-MM-yyyy") date: LocalDate,
+               @RequestParam(required = false, defaultValue = "01-01-2038")
+               @DateTimeFormat(pattern="dd-MM-yyyy") until: LocalDate,
+               @RequestParam columns: List<String>, @RequestParam colnames: List<String>,
+               model: Model) : String {
+        LOGGER.info("Generating report for expressions {}, colnames {}", columns, colnames)
+        val changesByInventory = columns.map { col ->
+            if(col.startsWith('-')) service.getAllExpensesSince(date, until, col.substring(1))
+            else service.getAllPurchasesSince(date, until, col.substring(1)) }
+        val changesByItem = HashMap<Item, Array<Change?>>()
+        for(i in changesByInventory.indices) {
+            changesByInventory[i].forEach {
+                if(changesByItem[it.item.item] == null) changesByItem[it.item.item] = arrayOfNulls(changesByInventory.size)
+                changesByItem[it.item.item]!![i] = it
+            }
+        }
+        val sortedMap : SortedMap<Item, List<Double>> = TreeMap (Comparator.comparing<Item, Int> { it.brPartije }
+                .then(Comparator.comparing<Item, Int> { it.brStavke }))
+        sortedMap.putAll(changesByItem.mapValues { changes -> changes.value.map { it?.amount ?: 0.0 } })
+        model["colnames"] = colnames
+        model["data"] = sortedMap
+        model["date"] = date.format(SLASH_FORMATTER)
+        model["untilDate"] = until.format(SLASH_FORMATTER)
+        return "report"
+    }
+
     private fun initAggregateModel(data: List<Change>, inventory: String, date: LocalDate, until: LocalDate, model: Model) {
-        val dateStr = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
-        val untilStr = until.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"))
+        val dateStr = date.format(DASH_FORMATTER)
+        val untilStr = until.format(DASH_FORMATTER)
         model["data"] = data
         model["aggregated"] = true
         model["inventory"] = inventory
@@ -145,4 +177,8 @@ class ViewController(@Autowired val service: CrudService) {
         service.resetInventory(name)
         return ResponseEntity.ok("Reset $name successfully!")
     }
+
+    //todo pregled izmena od - do
+    //todo pageable pregled izmena?
+    //todo tricolumn izmene - dodato na ugovore, prihodovano na stanju, rashodovano
 }
